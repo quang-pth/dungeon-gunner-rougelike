2d075db0895e22b819040ffa6de544590c321370
QuangPham <quang20201@gmail.com>
2022-05-27 12:24:21 +0700

    enemies prefabs set up

---
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+#region REQUIRE COMPONENTS
+[RequireComponent(typeof(SortingGroup))]
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CircleCollider2D))]
+[RequireComponent(typeof(PolygonCollider2D))]
+#endregion REQUIRE COMPONENTS
+[DisallowMultipleComponent]
+public class Enemy : MonoBehaviour
+{
+    [HideInInspector] public EnemyDetailsSO enemyDetailsSO;
+    private CircleCollider2D circleCollider2D;
+    private PolygonCollider2D polygonCollider2D;
+    [HideInInspector] public SpriteRenderer[] spriteRendererArray;
+
+    private void Awake() {
+        circleCollider2D = GetComponent<CircleCollider2D>();
+        polygonCollider2D = GetComponent<PolygonCollider2D>();
+        spriteRendererArray = GetComponentsInChildren<SpriteRenderer>();
+    }
+}
